90b4d26f1f6c9bf52f2f841da47f3bddf3532d87
rpc1418 <rudrakshchourey@outlook.com>
2023-10-20 18:01:36 +0530

    jhg

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://rpc1418.github.io/portfolio/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -19,6 +19,15 @@ function App() {
         </a>
       </header>
     </div>
+    // <> 
+    // <nav>
+    //   <li>home</li>
+    //   <li>about</li>
+    //   <li>start</li>
+    //   </nav>    
+    // <div className="blank">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus distinctio, ullam maiores inventore temporibus quo repudiandae maxime dolorem? Aliquid sed impedit possimus. Esse repellendus aliquam ipsa nesciunt sint dolorem? Alias impedit natus eaque laudantium, sequi dolorem laborum possimus voluptas est molestias quaerat officia eos sed similique ratione autem facere porro totam aspernatur sit qui. Placeat facere quae distinctio saepe repudiandae facilis dolorem rem sunt nulla ratione maxime fugit repellat corporis commodi nisi non unde error, sequi iure quidem minima nesciunt quaerat voluptas! Nobis molestiae, ratione repellat, corrupti itaque consequatur officiis error, nulla voluptas facilis veniam sed? Inventore id tenetur debitis.</div>
+    // </>
+     
   );
 }
 
